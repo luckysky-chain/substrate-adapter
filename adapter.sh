53a8f61c6415708a8f8174d2fd3168a6d27d16b4
fqmarkvon <fqmarkvon@gmail.com>
2021-12-21 13:13:35 -0500

    add configration file

---
+++ adapter.sh
@@ -0,0 +1,5 @@
+echo "SA_ENDPOINT=ws://54.209.240.158:8844" > substrate_adapter.env
+echo "SA_PRIVATE_KEY=0x02f7e19423a36e4e0a0e095e97e7edd2dc8d051ae9da00c457d524e54713b07b" >> substrate_adapter.env
+echo "SA_TX_TYPE=immortal" >> substrate_adapter.env
+
+
